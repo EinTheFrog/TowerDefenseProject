5b8b6ecafa882494432cdafc73d37b362a77d954
Maxim <alpatskij.mi@edu.spbstu.ru>
2021-06-02 00:20:23 +0300

    Closed levels

---
+++ Assets/Scripts/UI/LevelMenuBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelMenuBehaviour : MonoBehaviour
+{
+    private bool[] levelsStates = new bool[10];
+
+    private void Start()
+    {
+        levelsStates = new Boolean[10] {true, false, false, false, false, false, false, false, false, false};
+        var buttons = GetComponentsInChildren<Button>();
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (levelsStates[i] == false)
+            {
+                buttons[i].interactable = false;
+            }
+        }
+    }
+}
