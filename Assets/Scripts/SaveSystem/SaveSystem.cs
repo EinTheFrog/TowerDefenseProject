f75e5a2e6ffa0ef9cca12286b18d883ee6413a5a
Maxim <alpatskij.mi@edu.spbstu.ru>
2021-06-02 12:52:36 +0300

    saves drawback

---
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace SaveSystem
+{
+    public static class SaveSystem
+    {
+        private const string SAVE_FILE_NAME = "saveFile";
+
+        private static void SaveBoolArray( bool[] boolArr )
+        {
+            var savePath = Path.Combine(Application.persistentDataPath, SAVE_FILE_NAME);
+            FileStream fileStream = File.Open(savePath, FileMode.Create);
+            using (var writer = new BinaryWriter(fileStream))
+            {
+                foreach (var b in boolArr)
+                {
+                    writer.Write(b);
+                }
+            }
+        }
+
+        public static void SaveLevelsStates( bool[] levelsStates )
+        {
+            SaveBoolArray(levelsStates);
+        }
+        
+        private static bool[] LoadBoolArray( int length )
+        {
+            var savePath = Path.Combine(Application.persistentDataPath, SAVE_FILE_NAME);
+            FileStream fileStream;
+            try
+            {
+                fileStream = File.Open(savePath, FileMode.Open);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                return new bool[0];
+            }
+            
+            bool[] boolArr = new bool[length];
+            using (var reader = new BinaryReader(fileStream))
+            {
+                for (var i = 0; i < length; i++)
+                {
+                    boolArr[i] = reader.ReadBoolean();
+                }
+            }
+
+            return boolArr;
+        }
+
+        public static bool[] LoadLevelsStates( int length )
+        {
+            return LoadBoolArray(length);
+        }
+    }
+}
--- Assets/Scripts/UI/LevelMenuBehaviour.cs
+++ Assets/Scripts/UI/LevelMenuBehaviour.cs
@@ -6,15 +6,16 @@ using UnityEngine.UI;
 
 public class LevelMenuBehaviour : MonoBehaviour
 {
-    private bool[] levelsStates = new bool[10];
+    private const int LEVELS_AMOUNT = 10;
+    private bool[] _levelsStates = new bool[LEVELS_AMOUNT];
 
     private void Start()
     {
-        levelsStates = new Boolean[10] {true, false, false, false, false, false, false, false, false, false};
+        _levelsStates = SaveSystem.SaveSystem.LoadLevelsStates(LEVELS_AMOUNT);
         var buttons = GetComponentsInChildren<Button>();
-        for (int i = 0; i < buttons.Length; i++)
+        for (var i = 0; i < buttons.Length; i++)
         {
-            if (levelsStates[i] == false)
+            if (_levelsStates[i] == false)
             {
                 buttons[i].interactable = false;
             }
