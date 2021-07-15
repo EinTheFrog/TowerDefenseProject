f8bec2be1f5a4a0cf64d01f13a2874beb77d8f8a
EinTheFrog <MaximAlpatski@gmail.com>
2021-07-15 19:39:39 +0300

    level info

--- Assets/Scripts/UI/UIBehaviour.cs
+++ Assets/Scripts/UI/UIBehaviour.cs
@@ -2,20 +2,23 @@
 using SaveSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class UIBehaviour : MonoBehaviour
     {
-
-        private LevelManager _levelManager;
+        [SerializeField] private Text infoText = default;
+        
+        private LevelManager _levelManager = default;
+        private int _chosenLevelId = 0;
 
         private void Start()
         {
             _levelManager = FindObjectOfType<LevelManager>();
         }
 
-        public void StartGame(int levelId)
+        public void StartLevel(int levelId)
         {
             Time.timeScale = 1f;
             var levelName = "Level " + levelId;
@@ -34,6 +37,43 @@ namespace UI
             Time.timeScale = 1f;
             var levelName = "Level " + nextLevelId;
             SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+        }
+
+        public void ChooseLevel(int newId)
+        {
+            _chosenLevelId = newId;
+            string txt = "";
+            switch (newId)
+            {
+                case 0: txt = @"Welcome to the Horns and hooves industries. You will protect our great company from
+hacker attacks. They will try to steal our data using different viruses and you should
+destroy these viruses until we will define geolocation of these hackers and... 
+persuade them to stop attacking us."; 
+                    break;
+                case 1: txt = "Day 1"; 
+                    break;
+                case 2: txt = "Day 2"; 
+                    break;
+                case 3: txt = "Day 3"; 
+                    break;
+                case 4: txt = "Day 4"; 
+                    break;
+                case 5: txt = "Day 5"; 
+                    break;
+                case 6: txt = "Day 6"; 
+                    break;
+                case 7: txt = "Day 7"; 
+                    break;
+                case 8: txt = "Day 8"; 
+                    break;
+                case 9: txt = "Day 9"; 
+                    break;
+            }
+            infoText.text = txt;
+        }
+        public void StartChosenLevel()
+        {
+            StartLevel(_chosenLevelId);
         }
 
         public void GoToMainMenu()
