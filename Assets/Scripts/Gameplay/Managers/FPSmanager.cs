b6f4e51542232b5ad75c6ed54a3a52bdf570ab2f
EinTheFrog <MaximAlpatski@gmail.com>
2021-07-27 00:08:47 +0300

    ui res fix (houston)

---
+++ Assets/Scripts/Gameplay/Managers/FPSmanager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Managers
+{
+    public class FPSmanager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 40;
+        }
+    }
+}
